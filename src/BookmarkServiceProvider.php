e830dbe21e0ded99e7f67427df066f8debc80b42
jabber <2898117012@qq.com>
2020-08-01 19:33:20 +0800

    feat: bookmark

--- src/BookmarkServiceProvider.php
+++ src/BookmarkServiceProvider.php
@@ -1,8 +1,9 @@
 <?php
-/**
- * This file is part of the trendsoft/laravel-bookmark.
+
+/*
+ * This file is part of the trendsoft/laravel-bookmark
  * (c) trendsoft <hadi@trendsoft.org>
- * This source file is subject to the MIT license that is bundled.
+ * This source file is subject to the MIT license that is bundled with this source code in the file LICENER.
  */
 
 namespace LaravelBookmark;
--- src/Events/Bookmarked.php
+++ src/Events/Bookmarked.php
@@ -1,14 +1,13 @@
 <?php
-/**
- * This file is part of the trendsoft/laravel-bookmark.
+
+/*
+ * This file is part of the trendsoft/laravel-bookmark
  * (c) trendsoft <hadi@trendsoft.org>
- * This source file is subject to the MIT license that is bundled.
+ * This source file is subject to the MIT license that is bundled with this source code in the file LICENER.
  */
 
 namespace LaravelBookmark\Events;
 
-
 class Bookmarked extends Event
 {
-
 }
